963200dbfd5207401eae642fc67fe849c52c7c89
MalaikaSajid <malaikasajid05@gmail.com>
2026-05-16 14:06:33 +0500

    2nd commit

---
+++ src/Subtract.java
@@ -0,0 +1,12 @@
+public class Subtract {
+    int subtract = 0;
+    int a;
+    int b;
+    Subtract(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    int Sub() {
+        return a - b;
+    }
+}
